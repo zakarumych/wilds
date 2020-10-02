a2c4ed9462d7e2875d96cad0ef33bcb6e9863490
Zakarum <zakarumych@ya.ru>
2020-10-02 19:11:05 +0300

    Reconfigure sun and sky light

--- game/src/main.rs
+++ game/src/main.rs
@@ -63,15 +63,12 @@ fn main() -> Result<(), Report> {
         let mut renderer = Renderer::new(&window)?;
         let mut clocks = Clocks::new();
 
-        let sunlight = na::Vector3::new(255.0, 207.0, 72.0)
-            .map(|c| c / 255.0)
-            .map(|c| c / (1.3 - c));
+        let sunlight = na::Vector3::new(255.0, 207.0, 72.0);
         // .map(|c| c * 5.0);
 
         let skyradiance = na::Vector3::new(117.0, 187.0, 253.0)
             .map(|c| c / 255.0)
-            .map(|c| c / (1.3 - c));
-        // .map(|c| c * 5.0);
+            .map(|c| c / (1.1 - c));
 
         engine.world.spawn((
             DirectionalLight {
@@ -85,7 +82,7 @@ fn main() -> Result<(), Report> {
 
         engine.add_system(move |ctx: SystemContext<'_>| {
             let elapsed = ctx.clocks.step - ctx.clocks.start;
-            let d = elapsed.as_secs_f32() / 10.0;
+            let d = elapsed.as_secs_f32() / 2.0;
             let mut query = ctx.world.query::<&mut DirectionalLight>();
 
             for (_, dirlight) in query.iter() {
@@ -100,7 +97,7 @@ fn main() -> Result<(), Report> {
 
             for (_, skylight) in query.iter() {
                 skylight.radiance =
-                    (skyradiance * (1.2 - d.cos()) / 2.2).into();
+                    (skyradiance * (1.005 - d.cos()) / 2.005).into();
             }
         });
 
